cb8e44833d179bdea82828b87967c00737878949
Siddhi <manjrekarsiddhi72@gmail.com>
2025-07-21 23:30:06 +0530

    enable cors in backend

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ console.log("Loaded MONGO_URI:", process.env.MONGO_URI);
 connectDB(); // Connect to MongoDB
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
